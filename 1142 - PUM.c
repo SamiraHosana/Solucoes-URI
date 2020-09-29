fc2b37c78878a59089f476ae6b59411c9f7954ab
SamiraHosana <50033338+SamiraHosana@users.noreply.github.com>
2020-09-29 20:22:29 -0300

    Add files via upload

---
+++ 1142 - PUM.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+int N,*V,K,i,x;
+scanf("%d",&N);
+K=N*4;
+x=K;
+V=(int *)(malloc(K*sizeof(int)));
+for (i=0;i<K;i++){
+  V[i]=x--;
+}
+for (i=K-1;i>=0;i--){
+   
+  if (V[i]%4==0){
+  printf("PUM\n");
+  }
+  else{
+     printf("%d ",V[i]);
+  }
+
+}
+
+  return 0;
+}
