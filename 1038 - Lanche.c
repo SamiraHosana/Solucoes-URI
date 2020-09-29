84edb9803829205c4e54e7ecd6a1d14a40fc5d79
SamiraHosana <50033338+SamiraHosana@users.noreply.github.com>
2020-09-29 20:23:54 -0300

    Add files via upload

---
+++ 1038 - Lanche.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int c, q; 
+    float t;
+    
+    scanf("%d%d", &c,&q);
+    
+    switch(c){
+        case 1:
+        t=q*4.00;
+        break;
+        
+        case 2:
+        t=q*4.50;
+        break;
+        
+        case 3:
+        t=q*5.00;
+        break;
+        
+        case 4:
+        t=q*2.00;
+        break;
+        
+        case 5:
+        t=q*1.50;
+        break;
+    }
+    
+    printf("Total: R\$ %.2f\n",t);
+ 
+    return 0;
+}
